4a7148b54651bab45e01e04e29f9eea172ed1f8c
Junior Lawrence Kibirige <aanga26@gmail.com>
2022-01-02 12:41:47 +0300

    first commit

---
+++ app/src/main/java/lan/tmsystem/globochat/SettingsFragment.kt
@@ -0,0 +1,17 @@
+package lan.tmsystem.globochat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.PreferenceFragmentCompat
+
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+    }
+
+}
